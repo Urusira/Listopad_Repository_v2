428a9cd9e6c1ebbbc5fbb4848807eab892d9c7c3
Urusira <Listopadvadim@yandex.ru>
2023-10-30 14:20:49 +0300

    Revert "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
    This reverts commit 119fd8f0dc2c7873131f4099f75ab01fd627e40c.
    
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 /* main.c */
 #include <stdio.h>
+<<<<<<< HEAD
 #include "askname.h"
 int main(int argc, char **argv)
 {
- char first[255], last[255];
- askname(first, last);
- printf("Hello, %s %s!\n", first, last);
+ printf("Hello world!\n");
  return 0;
 }
